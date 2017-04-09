2782cd39b5b19c1276393efc5a4768167a0dc215
Apeksha <vivekhakim@Viveks-MacBook-Air.local>
2017-04-09 10:39:52 -0400

    Procfile added

--- server.js
+++ server.js
@@ -12,14 +12,18 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }))
 // override with POST having ?_method=DELETE
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
+
 var exphbs = require('express-handlebars');
+
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
+
 app.set('view engine', 'handlebars');
 
 var routes = require('./controllers/burgers_controller.js');
+
 app.use('/', routes);
 
 var port = process.env.port || 3000;
